ac5277a81174e956af279b41f833003367f55831
Godfadatun <danyadegoke@gmail.com>
2020-07-21 18:02:49 -0400

    controller updated

---
+++ controllers/deviceController.js
@@ -0,0 +1,41 @@
+
+var device_footprint = require('../models').device_footprint;
+
+// function load(req, res, next, id) {
+//   device_footprint.findById(id)
+//       .exec()
+//       .then((user) => {
+//         req.dbUser = user;
+//         return next();
+//       }, (e) => next(e));
+// }
+
+exports.getencripted = (req, res, next)=>{
+  device_footprint.findAll()
+    .then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: "success",
+        model: response
+      })
+    });
+}
+
+// this handles the logic for posting the info to the database
+exports.create_encription = (req, res, next)=>{
+  device_footprint.create({
+    ip_address: 'req.body.ip_address',
+    browser: 'req.body.browser',
+    os: 'req.body.os',
+    location: 'req.body.location',
+    encription: 'req.body.encription'
+  })
+  .then(response => {
+      res.json({
+        status: "success",
+        message: "success",
+        data: response
+      })
+  });
+}
--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var device_footprint = require('../models').device_footprint
+var deviceController = require('../controllers/deviceController');
 
 router.route('/device_footprint')
-  .get(function (req, res) {
-    device_footprint.findAll().then(response => {
-        // console.log("All users:", JSON.stringify(users, null, 4));
-      res.json({
-        status: "success",
-        message: "success",
-        data: "success",
-        model: response
-      })
-    });
-  })
-  .post(function (req, res) {
-    device_footprint.create({
-      ip_address: 'req.body.ip_address',
-      browser: 'req.body.browser',
-      os: 'req.body.os',
-      location: 'req.body.location',
-      encription: 'req.body.encription'
-    })
-    .then(response => {
-        res.json({
-          status: "success",
-          message: "success",
-          data: response
-        })
-    });
-  })
+  .get(deviceController.getencripted)
+  .post(deviceController.create_encription)
 
 module.exports = router;
